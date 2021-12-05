d4a0d4fda18d862228e1e864448baa63e021f6d3
Joseph Esfandiari <josephesfandiari@gmail.com>
2021-12-05 11:46:40 -0500

    Functioning

--- app.py
+++ app.py
@@ -4,13 +4,25 @@ import requests
 
 app = Flask(__name__)
 
-# url = "https://data.nasdaq.com/api/v3/datatables/ETFG/FUND.json?ticker=SPY&api_key=AxG8LscgzpqxLLHjrfa4"
-url = "https://data.nasdaq.com/api/v3/datasets/FRED/NROUST.json?api_key=AxG8LscgzpqxLLHjrfa4"
+url = "https://data.nasdaq.com/api/v3/datatables/ETFG/FUND.json?ticker=SPY&api_key=AxG8LscgzpqxLLHjrfa4"
+url2 = "https://data.nasdaq.com/api/v3/datasets/FRED/NROUST.json?api_key=AxG8LscgzpqxLLHjrfa4"
 
 @app.route("/")
 def index():
     response = requests.get(url)
     json_data = response.json()
-    important= (json_data["datatable"]["name"]["description"])
-    return(json_data)
+    important = (json_data)
+    return render_template(
+        "index.html",
+        importants = important,
+    )
+@app.route("/datasets")
+def datasets():
+    response = requests.get(url2)
+    json_data = response.json()
+    important = (json_data)
+    return render_template(
+        "index.html",
+        importants = important
+    )
 app.run(debug=True)
