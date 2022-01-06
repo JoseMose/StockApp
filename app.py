ad52ab38cb79cd781a698d3e6c20c55dbc98270d
Joseph Esfandiari <josephesfandiari@gmail.com>
2022-01-06 17:45:04 -0500

    Pushing it to the limit

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, json, render_template
 import os
 import requests
+import finnhub
+import content
 
 app = Flask(__name__)
 
 url = "https://data.nasdaq.com/api/v3/datatables/ETFG/FUND.json?ticker=SPY&api_key=AxG8LscgzpqxLLHjrfa4"
 url2 = "https://data.nasdaq.com/api/v3/datasets/FRED/NROUST.json?api_key=AxG8LscgzpqxLLHjrfa4"
 
+
 @app.route("/")
 def index():
+    finnhub_client = finnhub.Client(api_key="c6qdd2iad3i891nj3ov0")
     response = requests.get(url)
     json_data = response.json()
     important = (json_data)
+    covid = finnhub_client.covid19()
+    print(covid)
     return render_template(
         "index.html",
         importants = important,
@@ -22,7 +28,7 @@ def datasets():
     json_data = response.json()
     important = (json_data)
     return render_template(
-        "index.html",
+        "datasets.html",
         importants = important
     )
 app.run(debug=True)
